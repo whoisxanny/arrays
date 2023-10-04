e24c67f6e26a785613862ae59484e8fcef05e01b
whoisxanny <tuffx4nny@gmail.com>
2023-10-04 17:34:44 +0500

    homeWork2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
 public class Main {
-    public static void main(String[] args) {
-        int[] weights = {90, 93, 95, 99, 93, 95, 91, 94, 0, 0, 0, 0};
-        weights[0] = 90;
-        int januaryWeight = weights[0];
-        System.out.println(januaryWeight);
-        System.out.println(weights[0]);
-        int january = 0;
-        System.out.println(january);
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Summa zatrat za mesyac ravna " + sum + " rubley");
+
+
+
+        int max = -1;
+        for (int current : arr) {
+            if (current > max) {
+                max = current;
+            }
+        }
+        System.out.println("Maksimal'naya summa zatrat za den' ravna " + max + " rubley");
+
+        int min = 200000;
+        for (int current2 : arr) {
+            if (current2 < min) {
+                min = current2;
+            }
+        }
+        System.out.println("Minimal'naya summa zatrat za den' ravna " + min + " rubley");
+
+
+
+        int srednyaya = sum / 30;
+        System.out.println("Srednyaya summa zatrat za mesyac sostavila " + srednyaya + " rubley");
+
+
+
+
 
     }
+
 }
